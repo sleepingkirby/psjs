18c0c2331deab4d228f7778d2a13b4d21ea759bb
sleepingkirby@gmail.com <sleepingkirby@gmail.com>
2021-01-03 16:08:14 +0800

    set reading and writing to settings on options page. Also made it so that you can pull apply list settings from global and clear it

--- options/options.js
+++ options/options.js
@@ -1,81 +1,4 @@
 
-function addNewRow( className ){
-var tbl=document.getElementsByClassName(className);
-var newIn="";
-}
-
-//convert text lines to obj
-function txtArToObj(str){
-var lines=str.split("\n");
-console.log(lines);
-var rtrn={};
-  for(let item of lines){
-    if(item && item !== "\n"){
-    var tokPos=item.indexOf('|');
-    var indx=tokPos>0?item.substr(0,tokPos):item;
-    var patt=item.substr(tokPos+1,item.length-tokPos);
-      if( tokPos < 0 || patt.length === 0 || patt == ""){
-      patt=null;
-      }
-    rtrn[indx]=patt;
-    }
-  }
-return rtrn;
-}
-
-//convert object to text lines
-function objToTxtAr(obj){
-var rtrn="";
-var nl="";
-  for(var key in obj){
-    if(obj[key] === undefined || obj[key] === null){
-    rtrn+=nl+key;
-    }
-    else{
-    rtrn+=nl+key+"|"+obj[key];
-    }
-  nl="\n";
-  }
-return rtrn;
-}
-
-
-function saveNotify( obj, str, appnd=false){
-    if(appnd){
-    obj.appendChild(document.createElement("br")); 
-    obj.appendChild(document.createTextNode(str)); 
-    }
-    else{
-      //clear all children
-      while(obj.firstChild){
-        obj.firstChild.remove();
-      }
-    obj.appendChild(document.createTextNode(str)); 
-    }
-}
-
-
-function genSelect(elId, arr){
-  if(!Array.isArray(arr)){
-  return false;
-  }
-
-let el = document.getElementById(elId);
-  if( el == null ){
-  return false;
-  } 
- 
-  arr.forEach((a) => {
-  var node = document.createElement("option");
-  var textnode = document.createTextNode(a);
-  node.appendChild(textnode);
-  el.appendChild(node);
-  });
-
-return true;
-}
-
-
 function setMsg(el){
     document.getElementById(el.getAttribute("msgId")).innerText=el.getAttribute("info");
     document.getElementById(el.getAttribute("msgId")).style=el.getAttribute("msgStyl");
@@ -145,9 +68,7 @@ function startListen(){
         var obj={};
         obj[e.target.name]=e.target.type=="checkbox"?e.target.checked:e.target.value;
           chrome.storage.local.set(obj, function(){
-            chrome.storage.local.get(null, function(e){
-            console.log(e);
-            });
+            //chrome.storage.local.get(null, function(e){console.log(e);});
           }); 
         break;
         }
@@ -159,9 +80,7 @@ function startListen(){
         var obj={};
         obj[tmpEl.name]=value;
           chrome.storage.local.set(obj, function(e){
-            chrome.storage.local.get(null, function(e){
-            console.log(e);
-            });
+            //chrome.storage.local.get(null, function(e){});
           });
         }
       break;
@@ -172,38 +91,11 @@ function startListen(){
         el.textContent=el.textContent+arr[1]+"\n";
         }
       break;
-      case 'savePref':
-      //grab settings, parse and enter into storage.local
-      var custList=document.getElementsByClassName('custListTxt')[0].value;
-      var custListObj=txtArToObj(custList);
-      var custDmnPat=document.getElementsByClassName('custDmnPatTxt')[0].value;
-      var custDmnPatObj=txtArToObj(custDmnPat);
-      var custDmnSty=document.getElementsByClassName('custDmnStyTxt')[0].value;
-      var custDmnStyObj=txtArToObj(custDmnSty);
-
-      var notif=document.getElementsByClassName('notify')[0];
-      notif.id='';
-      notif.innerHTML='';
-
-        //setting custom list
-        chrome.storage.local.set({custList: custListObj},saveNotify(notif, 'Ignore List saved.', false ));
-
-        //setting custom domain pattern list
-        chrome.storage.local.set({custDmnPatList: custDmnPatObj}, saveNotify(notif, 'Custom domains and patterns saved.', true ));
-
-        //setting custom domain style patterns list
-        chrome.storage.local.set({custDmnStyList: custDmnStyObj}, saveNotify(notif, 'Custom domains and styled patterns saved.', true ));
-
-
-      notif.id='fadeOut';
-        notif.addEventListener("animationend", ()=>{
-        notif.id='';
-        });
+      case 'clrApply':
+      clearApplySetting();
       break;
-      case 'addLstnTxtInptSbmt':
-      //var tmpEl=document.getElementById("addLstnTxtInpt");
-      //console.log(tmpEl);
-      //console.log(e.target);
+      case 'pllApply':
+      getApplySetting();
       break;
       default:
       break;
@@ -233,55 +125,50 @@ function startListen(){
 
 }
 
+//sets the form to values stored in chrome.storage.local
+function getSettings(){
+  chrome.storage.local.get(null,function(d){
+  var tmpEl;
+    for(let k in d){
+    tmpEl=document.getElementsByName(k);
+    
+      if(tmpEl.length>=1){
+      tmpEl=tmpEl[0];
+        if(tmpEl.hasAttribute("type") && tmpEl.type=="checkbox"){
+        tmpEl.checked=d[k];
+        }
+        else{
+        tmpEl.value=d[k];
+        }
+      }
+    } 
+  });
+}
 
-/*
-//getting saved settings
-chrome.storage.local.get(null,(item) => {
-
-    //set default
-    if(!item.hasOwnProperty('mnl')){
-    console.log('butWhyMod: manual setting doesn\'t exist. Setting default value.');
-    item={mnl: true};
-    chrome.storage.local.set({mnl: true});
-    }
-
-
-    //gets ignorelist and custom domain modal removal class
-    //also sets defaults if the variables doesn't exist.
-  var custList={};
-    if(item.hasOwnProperty('custList') === false){
-      chrome.storage.local.set({custList: {'mail.google.com': null, 'twitter.com': null }});
-    custList={'mail.google.com': null, 'twitter.com': null };
-    }
-    else{
-    custList=item.custList;
-    }
-
-  var custDmnPatList={};
-    if(!item.hasOwnProperty('custDmnPatList')){
-    chrome.storage.local.set({custDmnPatList: {'www.facebook.com':'_5hn6'}});
-    custDmnPatList={'www.facebook.com':'_5hn6'};
-    }
-    else{
-    custDmnPatList=item.custDmnPatList;
-    }
-
-  var custDmnStyList={};
-    if(!item.hasOwnProperty('custDmnStyList')){
-    chrome.storage.local.set({custDmnStyList: {}});
-    custDmnStyList={};
-    }
-    else{
-    custDmnStyList=item.custDmnStyList;
-    }
-
- //document.getElementsByClassName('custListTxt')[0].value=objToTxtAr(custList);
- //document.getElementsByClassName('custDmnPatTxt')[0].value=objToTxtAr(custDmnPatList);
- //document.getElementsByClassName('custDmnStyTxt')[0].value=objToTxtAr(custDmnStyList);
-});
-*/
+//clears the form in applyList
+function getApplySetting(){
+document.getElementById("applyLstEnbld").checked=document.getElementById("inptAddOnEn").checked;
+document.getElementById("applyLstBrkJs").checked=document.getElementById("breakJs").checked;
+document.getElementById("applyLstStpJs").checked=document.getElementById("stopJs").checked;
+document.getElementById("applyLstEvnt").checked=document.getElementById("prvntEventLstnr").checked;
+document.getElementById("applyLstXHR").checked=document.getElementById("prvntXHR").checked;
+document.getElementById("applyLstEvntCst").value=document.getElementById("addLstnTxtInpt").value;
+document.getElementById("applyLstNtwrk").value=document.getElementById("addNtwrkTxtInpt").value;
+}
 
 
+//clears the form in applyList
+function clearApplySetting(){
+document.getElementById("applyLstEnbld").checked=true;
+document.getElementById("applyLstDmn").value="";
+document.getElementById("applyLstBrkJs").checked=false;
+document.getElementById("applyLstStpJs").checked=false;
+document.getElementById("applyLstEvnt").checked=false;
+document.getElementById("applyLstXHR").checked=false;
+document.getElementById("applyLstEvntCst").value="";
+document.getElementById("applyLstNtwrk").value="";
+}
+
+getSettings();
 //running main function
-
 startListen();
